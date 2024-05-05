faa7dcce5c0ebe246e63dfdb5e6edf9198f0e6b3
Giovanna Gamba <giovannagamba9@gmail.com>
2024-05-05 15:32:19 -0300

    Cria Controller

---
+++ src/server/AGP.MultiSearch/AGP.MultiSearch.API/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using AGP.MultiSearch.API.Dtos;
+using AGP.MultiSearch.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace AGP.MultiSearch.API.Controllers;
+
+[Route("main")]
+[ApiController]
+public class MainController : ControllerBase
+{
+    private readonly IMainService _mainService;
+
+    public MainController(IMainService mainService)
+    {
+        _mainService = mainService;
+    }
+
+    [HttpGet()]
+    public IActionResult Get([FromQuery] string textToSearch)
+    {
+        try
+        {
+            SearchObjectsDto dto = _mainService.Search(textToSearch);
+            return Ok(dto);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        }
+    }
+}
