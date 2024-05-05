77b7ceb30506c551b54146c465521bb6f9abd30b
Giovanna Gamba <giovannagamba9@gmail.com>
2024-05-05 15:26:56 -0300

    Cria Dto

---
+++ src/server/AGP.MultiSearch/AGP.MultiSearch.API/Dtos/SearchObjectsDto.cs
@@ -0,0 +1,21 @@
+﻿using AGP.MultiSearch.API.Models;
+
+namespace AGP.MultiSearch.API.Dtos;
+
+public class SearchObjectsDto
+{
+    public List<Equipment> Equipments { get; set; }
+    public List<Material> Materials { get; set; }
+    public List<PurchaseOrder> PurchaseOrders { get; set; }
+    public List<SaleOrder> SaleOrders { get; set; }
+    public List<WorkForce> WorkForces { get; set; }
+
+    public SearchObjectsDto()
+    {
+        Equipments = new List<Equipment>();
+        Materials = new List<Material>();
+        PurchaseOrders = new List<PurchaseOrder>();
+        SaleOrders = new List<SaleOrder>();
+        WorkForces = new List<WorkForce>();
+    }
+}
